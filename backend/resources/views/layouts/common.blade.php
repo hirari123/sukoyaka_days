fdf73384ef5768b6168ef9964c0b9e579c20012c
平川祥太 <shota@hirakawashoutanoMacBook-Pro.local>
2020-12-02 21:05:14 +0900

    blade_base

---
+++ backend/resources/views/layouts/common.blade.php
@@ -0,0 +1,18 @@
+<!-- 全ての画面のテンプレート -->
+
+<!DOCTYPE HTML>
+<html lang="ja">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="csrf-token" content="{{ csrf_token() }}">
+        <title>健やかデイズ</title>
+        <!-- cssをインポート -->
+        <link href="{{ mix('css/app.css') }}" rel="stylesheet" type="text/css">
+    </head>
+    <body>
+        @include('parts.header')
+        @yield('content')
+        <script src="{{ mix('js/app.js') }}"></script>
+    </body>
+</html>
