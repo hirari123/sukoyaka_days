b99be2c1a81ac46bd44fbf668c81564ed12d77e0
平川祥太 <shota@hirakawashoutanoMacBook-Pro.local>
2020-12-06 11:56:47 +0900

    バリデーションの表示を作成

--- backend/resources/views/auth/drafts/new.blade.php
+++ backend/resources/views/auth/drafts/new.blade.php
@@ -6,8 +6,23 @@
 
 <form action="/drafts/new" class="post-page-wrapper" method="post">
 @csrf
+  <!-- バリデーション title -->
+  @if($errors->first('title'))
+    <div class="validation">{{$errors->first('title')}}</div>
+  @endif
   <input type="text" class="form-control m-1" id="titl-input" placeholder="タイトル" name="title">
+
+  <!-- バリデーション tags -->
+  @if($errors->first('tags'))
+    <div class="validation">{{$errors->first('tags')}}</div>
+  @endif
   <input type="text" class="form-control m-1" placeholder="腰痛対策に関するタグをスペース区切りで3つまで入力" name="tags">
+
+  <!-- バリデーション body -->
+  @if($errors->first('body'))
+    <div class="validation">{{$errors->first('article')}}</div>
+  @endif
+
   <div class="row">
     <div class="col-6 m-1">
       <textarea name="article" id="markdown_editor_textarea" cols="30" rows="10" class="form-control"></textarea>
