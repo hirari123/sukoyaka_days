e54238bfda8ea20a8f69eee46a13488215ac7ae8
平川祥太 <shota@hirakawashoutanoMacBook-Pro.local>
2020-12-12 11:21:20 +0900

    showArticleメソッドの変更

--- backend/routes/web.php
+++ backend/routes/web.php
@@ -17,7 +17,7 @@ use App\Http\Controllers\Auth\PostController;
 // トップページを表示
 Route::get('/', 'App\Http\Controllers\Auth\PostController@showTopPage')->name('top');
 
-Route::get('/', [PostController::class, 'showTopPage']);
+// Route::get('/', [PostController::class, 'showTopPage']);
 
 
 Auth::routes();
@@ -25,17 +25,17 @@ Route::get('/home', 'HomeController@index')->name('home');
 
 Route::get('/drafts/new', 'App\Http\Controllers\Auth\PostController@index')->name('drafts.new');
 
-Route::get('/', [PostController::class, 'index']);
+Route::get('/', [PostController::class, 'index'])->name('drafts.new');
 
 // 記事を投稿する時、postリクエストを送る
 Route::post('/drafts/new', 'App\Http\Controllers\Auth\PostController@postArticle')->name('drafts.new.posts');
 
-Route::get('/', [PostController::class, 'postArticle']);
+Route::get('/', [PostController::class, 'postArticle'])->name('drafts.new.posts');
 
 // 実際に投稿された記事の内容を表示
 Route::get('/drafts/{id}', 'App\Http\Controllers\Auth\PostController@showArticle')->name('item');
 
-Route::get('/', [PostController::class, 'showArticle']);
+Route::get('/', [PostController::class, 'showArticle'])->name('item');
 
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
